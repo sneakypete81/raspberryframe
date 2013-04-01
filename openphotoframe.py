edc88f66d64a105a580cf22da5a67ed54db1f2b2
Pete <sneakypete81@gmail.com>
2013-04-01 18:31:26 +0100

    Perform "proper" photo shuffling, without replacement

--- openphotoframe.py
+++ openphotoframe.py
@@ -8,19 +8,23 @@ import keys
 class OpenPhotoFrame:
     def __init__(self, width, height, cache_path, cache_size_mb):
         self.openphoto = openphoto.OpenPhoto(keys.host, *keys.auth)
-        self.num_photos = None
         self.width = width
         self.height = height
         self.cache_path = cache_path
         self.cache_size_mb = cache_size_mb
+        self.shuffled_photos = []
+        self.current_photo_index = 0
 
         if not os.path.exists(cache_path):
             os.makedirs(cache_path)
 
-        self._update_num_photos()
+        self._randomise()
 
-    def _update_num_photos(self):
-        self.num_photos = self.openphoto.photos.list(pageSize=1)[0].totalPages
+    def _randomise(self):
+        self.current_photo_index = 0
+        num_photos = self.openphoto.photos.list(pageSize=1)[0].totalPages
+        self.shuffled_photos = range(num_photos)
+        random.shuffle(self.shuffled_photos)
 
     def get_image(self, photo):
         url = photo.get_fields()["path%dx%d" % (self.width, self.height)]
@@ -54,12 +58,14 @@ class OpenPhotoFrame:
             os.remove(filepath)
 
     def random_image(self):
-        page = random.randint(1, self.num_photos)
-        print "Preparing image..."
-        photo = self.openphoto.photos.list(pageSize=1, page=page, 
+        page = self.shuffled_photos[self.current_photo_index]
+        photo = self.openphoto.photos.list(pageSize=1, page=page,
                                            returnSizes="%sx%s" % (self.width, self.height))[0]
-        # Update the number of photos, if necessary
-        if photo.totalPages != self.num_photos:
-            self._update_num_photos()
+        self.current_photo_index += 1
+
+        # Re-randomise if necessary
+        if (self.current_photo_index >= len(self.shuffled_photos) or
+            photo.totalPages != len(self.shuffled_photos)):
+            self._randomise()
 
         return self.get_image_cached(photo)
