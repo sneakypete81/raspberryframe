a72fb5ce2ec48ff3ac066abdf81182490675caaa
Pete <sneakypete81@gmail.com>
2013-04-14 18:08:35 +0100

    My personal touchscreen setup file

---
+++ touchscreen_setup.sh
@@ -0,0 +1,8 @@
+export LD_LIBRARY_PATH=/usr/local/lib
+
+export TSLIB_CALIBFILE=/home/pi/pointercal
+export TSLIB_PLUGINDIR=/usr/local/lib/ts
+export TSLIB_CONFFILE=/usr/local/etc/ts.conf
+export TSLIB_TSDEVICE=/dev/input/event1
+export TSLIB_FBDEVICE=/dev/fb1
+export TSLIB_CONSOLEDEVICE=none
