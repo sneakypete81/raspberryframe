99618bc2533cdf9666e4cc79c3f2f27e5804a14d
sneakypete81 <sneakypete81@gmail.com>
2013-07-14 17:13:47 +0100

    Remove background alpha fill, since it's a bit slow when rendering to a DisplayLink device

--- overlay.py
+++ overlay.py
@@ -8,33 +8,18 @@ class LayeredButton(sgc.Button):
     """
     _layered = True
 
-# TODO: Convert header/footer into themed images
-class Background(sgc.Simple):
-    _surf_flags = pygame.SRCALPHA
-
-    def __init__(self, theme):
-        surf = pygame.Surface((theme.screen_width, theme.screen_height),
-                               self._surf_flags)
-        surf.fill((16, 16, 16, theme.header_alpha),
-                  rect=(theme.header_pos, theme.header_size))
-        surf.fill((16, 16, 16, theme.footer_alpha),
-                  rect=(theme.footer_pos, theme.footer_size))
-        sgc.Simple.__init__(self, surf)
-
 class Overlay:
     def __init__(self, theme):
         self.theme = theme
         self._is_active = False
 
-        self.bg = Background(theme)
         self.back = LayeredButton(widget=self, surf=theme.back_button,
                                   pos=theme.back_pos)
         self.forward = LayeredButton(widget=self, surf=theme.forward_button,
                                      pos=theme.forward_pos)
         self.star = LayeredButton(widget=self, surf=theme.unstarred_button,
                                   pos=theme.star_pos)
-        self.widgets = [self.bg,
-                        self.back, self.forward, self.star]
+        self.widgets = [self.back, self.forward, self.star]
 
     def add(self, fade=True, fade_delay=1):
         self._is_active = True
